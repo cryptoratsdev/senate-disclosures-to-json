34567fa27a3d929a10106a5048e9ee29015ae557
Max Gonzih <gonzih@gmail.com>
2022-01-07 13:11:48 -0800

    Scrape all years

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 func main() {
-	ry := NewYear("2021")
-	data := ry.Data()
-	for _, disc := range data.Disclosures {
-		disc.DocString(ry.Year)
+	years := []string{
+		"2008",
+		"2009",
+		"2010",
+		"2011",
+		"2012",
+		"2013",
+		"2014",
+		"2015",
+		"2016",
+		"2017",
+		"2018",
+		"2019",
+		"2020",
+		"2021",
+		"2022",
+	}
+
+	for _, year := range years {
+		ry := NewYear(year)
+		data := ry.Data()
+		for _, disc := range data.Disclosures {
+			disc.DocString(ry.Year)
+		}
 	}
 }
